019e5a4c43c064339b00c67085c873a31cfab900
mastr777 <mstrandberg777@gmail.com>
2026-03-19 15:34:00 +0100

    add expanded function. . some adjustments

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ function checkWord(word1, word2) {
   for (let i = 0; i < word1.length; i++) {
 
     if (word1[i] === word2[i]) {
-      result.push({ letter: word1[i], result: "correct" });
+        result.push({ letter: word1[i], result: "rätt bokstav och rätt plats" });
+    } else if (word2.includes(word1[i])) {
+        result.push({ letter: word1[i], result: "rätt bokstav men fel plats" });
     } else {
-      result.push({ letter: word1[i], result: "incorrect" });
+        result.push({ letter: word1[i], result: "finns ej"});
     }
     
   }
