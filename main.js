84d8478a174f0acfd5dd78157f4700397a5e49d5
mastr777 <mstrandberg777@gmail.com>
2026-03-19 15:15:00 +0100

    add first function

---
+++ main.js
@@ -0,0 +1,23 @@
+// wordle algoritm - start
+
+function checkWord(word1, word2) {
+  let result = [];
+
+  for (let i = 0; i < word1.length; i++) {
+
+    if (word1[i] === word2[i]) {
+      result.push({ letter: word1[i], result: "correct" });
+    } else {
+      result.push({ letter: word1[i], result: "incorrect" });
+    }
+    
+  }
+
+  return result;
+}
+
+
+
+
+
+
