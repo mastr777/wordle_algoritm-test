5eb41b9769fd36e030266b96eedfce8534852a3e
mastr777 <mstrandberg777@gmail.com>
2026-03-20 13:29:46 +0100

    adjust the code as two blocks - one if correct - one for if misplaced or incorrect

--- checkWord.js
+++ checkWord.js
@@ -1,18 +1,37 @@
 // wordle algoritm - start
 
 function checkWord(word1, word2) {
+
   let result = [];
+  let lettersLeftover = word2.split('');
+
+  // hitta korrekt bokstav
 
   for (let i = 0; i < word1.length; i++) {
 
     if (word1[i] === word2[i]) {
-        result.push({ letter: word1[i], result: "correct" });
-    } else if (word2.includes(word1[i])) {
-        result.push({ letter: word1[i], result: "misplaced" });
+        result.push({letter: word1[i], result: 'correct'});
+        lettersLeftover[i] = null;
     } else {
-        result.push({ letter: word1[i], result: "incorrect"});
+        result.push({letter: word1[i], result: ''});
     }
     
+  }
+
+  // hitta felplacerad eller fel bokstav
+
+  for (let i = 0; i < word1.length; i++) {
+    if (result[i].result === '') {
+      let index = lettersLeftover.indexOf(word1[i]);
+
+      if (index !== -1) {
+        result[i].result = 'misplaced';
+        lettersLeftover[index] = null;
+      } else {
+        result[i].result = 'incorrect';
+      }
+      
+    }
   }
 
   return result;
